18b850be4e376a2f1a441db2707b906f3ed74666
Ubuntu <ubuntu@ip-172-31-91-94.ec2.internal>
2019-10-29 17:16:17 +0000

    Initial commit

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,23 +1,23 @@
 import os
+import datetime
 import pymysql
 
-#get username from cloud9 workspace
-#(modify this variable if running on another environment)
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
-#connect to the database
+# Connect to the database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='Chinook')
-                            
+                             user=username,
+                             password='',
+                             db='Chinook')
+
 try:
-    #Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        rows = [("bob", 21, "1990-02-06 23:04:56"),
+                ("jim", 56, "1955-05-09 13:12:45"),
+                ("fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
+        connection.commit()
 finally:
-    #Close the connection , regardless of whether the above was successful
     connection.close()
